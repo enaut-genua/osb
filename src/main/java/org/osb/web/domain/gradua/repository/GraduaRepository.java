e7557320786563d09d8dd8f42a70949224ab0427
Eñaut Genua Prieto <enaut.genua@alumni.mondragon.edu>
2023-06-11 18:07:47 +0200

    garbitu kodea

--- src/main/java/org/osb/web/domain/gradua/repository/GraduaRepository.java
+++
@@ -1,9 +0,0 @@
-package org.osb.web.domain.gradua.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import org.osb.web.domain.gradua.model.Gradua;
-
-public interface GraduaRepository extends CrudRepository<Gradua, Long> {
-
-}
--- src/main/java/org/osb/web/domain/irakaslea/repository/IrakasleaRepository.java
+++
@@ -1,9 +0,0 @@
-package org.osb.web.domain.irakaslea.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import org.osb.web.domain.irakaslea.model.Irakaslea;
-
-public interface IrakasleaRepository extends CrudRepository<Irakaslea, Long> {
-    
-}
